d67f57ab10191611f0e57c7ccc06577d9afec334
Walber Fellipe <walberfellipe18@gmail.com>
2025-02-26 20:35:08 -0300

    feature: removed any from code

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -1,21 +1,48 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import SwaggerUI from 'swagger-ui-react'
-import 'swagger-ui-react/swagger-ui.css'
+import { useEffect, useState } from "react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+interface OpenAPIInfo {
+  title: string;
+  version: string;
+}
+
+interface OpenAPIPath {
+  [key: string]: {
+    [method: string]: {
+      summary?: string;
+      description?: string;
+      parameters?: Array<{
+        name: string;
+        in: string;
+        required: boolean;
+        type: string;
+      }>;
+      responses?: Record<string, { description: string }>;
+    };
+  };
+}
+
+interface OpenAPISpec {
+  openapi: string;
+  info: OpenAPIInfo;
+  paths: OpenAPIPath;
+}
 
 export default function ApiDocs() {
-  const [spec, setSpec] = useState<any>(null)
+  const [spec, setSpec] = useState<OpenAPISpec | null>(null);
 
   useEffect(() => {
-    fetch('/api/docs')
-      .then(res => res.json())
-      .then(setSpec)
-  }, [])
+    fetch("/api/docs")
+      .then((res) => res.json())
+      .then((data: OpenAPISpec) => setSpec(data));
+  }, []);
 
   if (!spec) {
-    return <div>Carregando...</div>
+    return <div>Carregando...</div>;
   }
 
-  return <SwaggerUI spec={spec} />
-} 
+  return <SwaggerUI spec={spec} />;
+}
