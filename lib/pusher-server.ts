2aaf395d1ff7d31623d61b561a9bd80924cfe499
Walber Fellipe <walberfellipe18@gmail.com>
2025-02-26 22:42:22 -0300

    feature: fixed types for next version

--- lib/pusher-server.ts
+++ lib/pusher-server.ts
@@ -1,10 +1,6 @@
-import { pusherServer } from './pusher'
+import { pusher } from './pusher'
 import { RoomEvent } from './realtime'
 
 export async function pushToRoom(roomId: string, event: RoomEvent) {
-  await pusherServer.trigger(
-    `room-${roomId}`,
-    event.type,
-    event.data
-  )
+  await pusher.trigger(`room-${roomId}`, event.type, event.data);
 } 
