ff694d301b131929baf02234158f74c02be50477
Walber Fellipe <walberfellipe18@gmail.com>
2025-03-08 19:07:31 -0300

    fix: a valid type for build

--- app/api/rooms/[roomId]/card-selected/route.ts
+++ app/api/rooms/[roomId]/card-selected/route.ts
@@ -5,16 +5,17 @@ import { pusher } from "@/lib/pusher";
 
 export async function POST(
   request: Request,
-  { params }: { params: { roomId: string } }
+  props: { params: Promise<{ roomId: string }> }
 ) {
+  const { roomId } = await props.params;
   try {
     const session = await getServerSession(authOptions);
     const { participantId, storyId } = await request.json();
 
-    await pusher.trigger(`room-${params.roomId}`, "card:selected", {
+    await pusher.trigger(`room-${roomId}`, "card:selected", {
       participantId,
       userId: session?.user?.id,
-      storyId
+      storyId,
     });
 
     return NextResponse.json({ success: true });
