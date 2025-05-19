f95cfa938bb718bc3a1ec5f3bb253bd6e756e9a8
Walber Fellipe <walberfellipe18@gmail.com>
2025-05-18 23:17:36 -0300

    feat: added a delay to call pusher instance

--- lib/pusher-server.ts
+++ lib/pusher-server.ts
@@ -1,6 +1,7 @@
-import { pusher } from './pusher'
-import { RoomEvent } from './realtime'
+import { getPusherServerInstance } from "./pusher";
+import { RoomEvent } from "./realtime";
 
+const pusher = getPusherServerInstance();
 export async function pushToRoom(roomId: string, event: RoomEvent) {
   await pusher.trigger(`room-${roomId}`, event.type, event.data);
-} 
+}
--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,18 +1,20 @@
-import Pusher from 'pusher'
-import PusherClient from 'pusher-js'
+// lib/pusher.ts
+import Pusher from "pusher";
+import PusherClient from "pusher-js";
 
-export const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-  useTLS: true
-})
+export function getPusherServerInstance() {
+  return new Pusher({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    useTLS: true,
+  });
+}
 
-export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_KEY!,
-  {
+export function getPusherClientInstance() {
+  return new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
     forceTLS: true,
-  }
-)
+  });
+}
